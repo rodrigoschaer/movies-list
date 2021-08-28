2691b6343447672b871e5126d79897b018ecb1cb
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2021-08-28 14:47:50 -0300

    added typescript and sass

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.scss";
 
 const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
